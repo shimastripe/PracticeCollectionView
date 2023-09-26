35d565c86f489f9b6547021d8b455b0a744a3af7
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2023-09-27 03:46:07 +0900

    Add TableOfContentsViewController

---
+++ PracticeCollectionView/TableOfContentsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableOfContentsViewController.swift
+//  PracticeCollectionView
+//
+//  Created by shimastripe on 2023/09/27.
+//
+
+import UIKit
+
+final class TableOfContentsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
