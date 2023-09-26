8fce2adde59146d2accea26e82b03ac4a60f0a71
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2023-09-27 03:34:46 +0900

    Initial Commit

---
+++ PracticeCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeCollectionView
+//
+//  Created by shimastripe on 2023/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
